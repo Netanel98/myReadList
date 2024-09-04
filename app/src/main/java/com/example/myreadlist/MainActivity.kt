e5146d8448ba56bdbc7e9779a1b57db0f3540306
Netanel98 <netanelsoussan@gmail.com>
2024-09-04 12:12:14 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/myreadlist/MainActivity.kt
+++ app/src/main/java/com/example/myreadlist/MainActivity.kt
@@ -1,26 +1,5 @@
 package com.example.myreadlist
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
+class MainActivity () {
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Set up navigation controller with the navHostFragment
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        return navController.navController.navigateUp() || super.onSupportNavigateUp()
-    }
 }
